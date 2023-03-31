f8bcc2df999c2dce0d149096bda0ba3aeba5ead8
saif ivna alam <alam.saifivna@gmail.com>
2023-03-31 11:10:30 +0600

    updated : assets

--- lib/shared/utils/assets.dart
+++ lib/shared/utils/assets.dart
@@ -2,3 +2,5 @@ String sidebarImage =
     "https://img.freepik.com/premium-vector/fork-spoon-restaurant-logo-vector-template_736856-30.jpg?w=2000";
 String noodlesImage =
     "https://bellyfull.net/wp-content/uploads/2022/08/Chow-Mein-blog-1.jpg";
+String personImagePNG =
+    "https://www.shutterstock.com/image-photo/young-handsome-man-beard-wearing-260nw-1768126784.jpg";
