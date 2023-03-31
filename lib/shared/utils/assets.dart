0a74c771e5c9814911fed1a741041c03b72a95c9
saif ivna alam <alam.saifivna@gmail.com>
2023-03-31 08:17:04 +0600

    added : assets

--- lib/shared/utils/assets.dart
+++ lib/shared/utils/assets.dart
@@ -1,2 +1,4 @@
 String sidebarImage =
     "https://img.freepik.com/premium-vector/fork-spoon-restaurant-logo-vector-template_736856-30.jpg?w=2000";
+String noodlesImage =
+    "https://bellyfull.net/wp-content/uploads/2022/08/Chow-Mein-blog-1.jpg";
