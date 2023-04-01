9ee906020570d0d32006db28a464ed54f806d0c7
saif ivna alam <alam.saifivna@gmail.com>
2023-04-01 19:26:52 +0600

    updated: assets

--- lib/shared/utils/assets.dart
+++ lib/shared/utils/assets.dart
@@ -4,3 +4,5 @@ String noodlesImage =
     "https://bellyfull.net/wp-content/uploads/2022/08/Chow-Mein-blog-1.jpg";
 String personImagePNG =
     "https://www.shutterstock.com/image-photo/young-handsome-man-beard-wearing-260nw-1768126784.jpg";
+String newOrderImagePNG =
+    "https://img.freepik.com/free-vector/restaurant-logo_23-2147506959.jpg?w=2000";
